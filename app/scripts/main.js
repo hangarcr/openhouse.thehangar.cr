b0e85db8a49788bcbc52729c7ee3955b424964fe
Jairo Gatjens <jgatjens@gmail.com>
2015-08-09 11:25:48 -0600

    Add custom bootstrap modal component, some updates to tablet devices

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,4 @@
 console.log('\'Allo \'Allo!');
+
+// onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=620,width=500');return false;",
+// href='https://twitter.com/share?text=#{encodeURIComponent(tags.title)}&via=#{tags.twitter}&url=#{encodeURIComponent(tags.url)}')
