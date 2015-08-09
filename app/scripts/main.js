fbb2643572b8fd100c0bdd5afcb638a692fe630b
Jairo Gatjens <jgatjens@gmail.com>
2015-08-09 11:39:31 -0600

    Updates tablet portrait, add fastclick vendor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,6 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+  FastClick.attach(document.body);
+});
 
 // onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=620,width=500');return false;",
 // href='https://twitter.com/share?text=#{encodeURIComponent(tags.title)}&via=#{tags.twitter}&url=#{encodeURIComponent(tags.url)}')
