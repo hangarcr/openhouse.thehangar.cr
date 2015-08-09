262503c1cd32780be4cb7f63a7a4bbe81d9d3370
Jairo Gatjens <jgatjens@gmail.com>
2015-08-09 13:21:38 -0600

    Update mobile checkbox, add animation marquee arrow down

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,18 @@
+// jsLint
+/*global FastClick $*/
+
 $(function() {
+  'use strict';
   FastClick.attach(document.body);
+
+  $(".js-h-arrow-down").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#intro").offset().top
+    }, 800);
+  });
+
+
 });
 
 // onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=620,width=500');return false;",
